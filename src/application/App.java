275e8cfb44735dfd6cbb191f6b9a165a552891c1
earl-stephens <stephensearl@hotmail.com>
2022-08-05 21:17:24 -0600

    Add very basic code for the File class

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,22 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		File currentDirectory = new File(".");
+		
+		System.out.println(currentDirectory.getAbsolutePath());
+		//shows working directory
+		
+		System.out.println(currentDirectory.getCanonicalPath());
+		
+		//show files in the directory
+		for(String f: currentDirectory.list()) {
+			System.out.println(f);
+		}
 
 	}
 
